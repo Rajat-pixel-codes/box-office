4a3ea187725c42a41e47d9cdffcaad0b54cb7ea2
Rajat Gupta <rajatsmart2001@gmail.com>
2022-07-25 13:53:51 +0530

    added navbar and created page components

---
+++ src/components/Navs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LINKS = [
+  { to: '/', Text: 'Home' },
+  { to: '/starred', Text: 'starred' },
+];
+
+function Navs() {
+  return (
+    <div>
+      <ul>
+        {LINKS.map(item => (
+          <li key={item.to}>
+            <Link to={item.to}>{item.Text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Navs;
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const starred = () => {
+  return <div>this is starred</div>;
+};
+
+export default starred;
